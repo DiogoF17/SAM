949536231e09a270c9e8f1f3b53de0872e49de00
Marcelo Reis <up201809566@fe.up.pt>
2022-06-02 12:04:53 +0100

    Update to database.dart.

--- app/lib/database/database.dart
+++ app/lib/database/database.dart
@@ -2,7 +2,6 @@ import 'package:firebase_database/firebase_database.dart';
 import "dart:convert";
 
 import "category.dart";
-import "media.dart";
 import "image.dart";
 import "sound.dart";
 
@@ -10,31 +9,20 @@ DatabaseReference databaseReference = FirebaseDatabase.instance.ref();
 
 void createDatabaseStructure(int numEntries) {
   for (int i = 0; i < numEntries; i++) {
-    DatabaseReference videoRef = databaseReference.child("videos").push();
-    videoRef.set({"name": "nd", "path": "nd"});
-
     DatabaseReference imgRef = databaseReference.child("images").push();
-    imgRef.set({"name": "nd", "path": "nd"});
+    imgRef.set({"name": "nd", "path": "nd", "soundId": "nd"});
 
     DatabaseReference soundRef = databaseReference.child("sounds").push();
     soundRef
-        .set({"name": "nd", "path": "nd", "imageId": "nd", "videoId": "nd"});
+        .set({"name": "nd", "path": "nd", "categoryId": "nd"});
 
     DatabaseReference categoriesRef =
         databaseReference.child("categories").push();
-    categoriesRef.set({"name": "nd", "mediaType": "nd"});
-
-    DatabaseReference categoriesSoundsRef =
-        databaseReference.child("categoriesSounds").push();
-    categoriesSoundsRef.set({"categoryId": "nd", "soundId": "nd"});
+    categoriesRef.set({"name": "nd", "hasSound": "nd"});
 
     DatabaseReference categoriesImagesRef =
         databaseReference.child("categoriesImages").push();
     categoriesImagesRef.set({"categoryId": "nd", "imageId": "nd"});
-
-    DatabaseReference categoriesVideosRef =
-        databaseReference.child("categoriesVideos").push();
-    categoriesVideosRef.set({"categoryId": "nd", "videoId": "nd"});
   }
 }
 
@@ -54,15 +42,6 @@ Future<List<Category>> loadCategories() async {
 
   return categories;
 }
-
-// Future<List<MyImage>> loadMedia(Category category) async {
-// if (category.mediaType == MediaType.image) {
-// return await loadImages(category);
-// }
-// else {
-//   return await loadVideos(category);
-// }
-// }
 
 Future<List<MyImage>> loadMedia(Category category) async {
   List<MyImage> images = [];
@@ -97,74 +76,32 @@ Future<List<MyImage>> loadMedia(Category category) async {
   return images;
 }
 
-// Future<List<MyImage>> loadImages(Category category) async {
-//   List<MyImage> images = [];
-
-//   Map<String, dynamic> JSON = await getAllDataFromTable("images");
-
-//   if (JSON.isNotEmpty) {
-//     var keys = JSON.keys;
-//     for (String key in keys) {
-//       if (JSON[key]["name"] != "nd") {
-//         images.add(MyImage.fromJSON(key, JSON[key]));
-//       }
-//     }
-//   }
-
-//   List<String> imageIdsToUse = [];
-
-//   JSON = await getAllDataFromTable("categoriesImages");
-
-//   if (JSON.isNotEmpty) {
-//     var keys = JSON.keys;
-//     for (String key in keys) {
-//       String imageId = JSON[key]["imageId"];
-//       String categoryId = JSON[key]["categoryId"];
-
-//       if (categoryId == category.id) imageIdsToUse.add(imageId);
-//     }
-//   }
-
-//   images.removeWhere((element) => !(imageIdsToUse.contains(element.id)));
-
-//   return images;
-// }
-
-// Future<List<Video>> loadVideos(Category category) async {
-//   List<Video> videos = [];
-
-//   Map<String, dynamic> JSON = await getAllDataFromTable("videos");
-
-//   if (JSON.isNotEmpty) {
-//     var keys = JSON.keys;
-//     for (String key in keys) {
-//       if (JSON[key]["name"] != "nd") {
-//         videos.add(Video.fromJSON(key, JSON[key]));
-//       }
-//     }
-//   }
-
-//   List<String> videoIdsToUse = [];
-
-//   JSON = await getAllDataFromTable("categoriesVideos");
-
-//   if (JSON.isNotEmpty) {
-//     var keys = JSON.keys;
-//     for (String key in keys) {
-//       String videoId = JSON[key]["videoId"];
-//       String categoryId = JSON[key]["categoryId"];
-
-//       if (categoryId == category.id) videoIdsToUse.add(videoId);
-//     }
-//   }
-
-//   videos.removeWhere((element) => !videoIdsToUse.contains(element.id));
-
-//   return videos;
-// }
-
 Future<List<Sound>> loadSounds(Category category) async {
   List<Sound> sounds = [];
+
+  Map<String, dynamic> JSON = await getAllDataFromTable("sounds");
+
+  if (JSON.isNotEmpty) {
+    var keys = JSON.keys;
+    for (String key in keys) {
+      if (JSON[key]["name"] != "nd") {
+        sounds.add(Sound.fromJSON(key, JSON[key]));
+      }
+    }
+  }
+
+  List<String> soundIdsToUse = [];
+
+  if (JSON.isNotEmpty) {
+    var keys = JSON.keys;
+    for (String key in keys) {
+      String categoryId = JSON[key]["categoryId"];
+
+      if (categoryId == category.id) soundIdsToUse.add(key);
+    }
+  }
+
+  sounds.removeWhere((element) => !(soundIdsToUse.contains(element.id)));
 
   return sounds;
 }
@@ -210,36 +147,17 @@ Future removeImageFromDB(String key) async {
   await removeTableRowByAttribute("categoriesImages", "imageId", key);
 }
 
-Future removeVideosFromDB(String key) async {
-  await removeTableRowByKey("videos", key);
-  await removeTableRowByAttribute("sounds", "videoId", key);
-  await removeTableRowByAttribute("categoriesVideos", "videoId", key);
-}
-
-void addImageToDB(String name, String path, String categoryId) {
+void addImageToDB(String name, String path, String categoryId, String soundId) {
   DatabaseReference imgRef = databaseReference.child("images").push();
-  imgRef.set({"name": name, "path": path});
+  imgRef.set({"name": name, "path": path, "soundId":soundId});
   DatabaseReference categoriesImagesRef =
       databaseReference.child("categoriesImages").push();
   categoriesImagesRef.set({"categoryId": categoryId, "imageId": imgRef.key});
 }
 
-void addVideoToDB(String name, String? path, String? categoryId) {
-  DatabaseReference videoRef = databaseReference.child("videos").push();
-  videoRef.set({"name": name, "path": path});
-  DatabaseReference categoriesVideosRef =
-      databaseReference.child("categoriesVideos").push();
-  categoriesVideosRef.set({"categoryId": categoryId, "videoId": videoRef.key});
-}
-
-void addSoundToDB(String name, String path, String imageId, String videoId,
-    String categoryId) {
+void addSoundToDB(String name, String path, String categoryId) {
   DatabaseReference soundRef = databaseReference.child("sounds").push();
-  soundRef.set(
-      {"name": name, "path": path, "imageId": imageId, "videoId": videoId});
-  DatabaseReference categoriesSoundsRef =
-      databaseReference.child("categoriesSounds").push();
-  categoriesSoundsRef.set({"categoryId": categoryId, "soundId": soundRef.key});
+  soundRef.set({"name": name, "path": path, "categoryId": categoryId});
 }
 
 void runDBOperations() {}
